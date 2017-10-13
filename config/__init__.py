626fb2d3fc2250e61bbbfc98e7eb987a23ca142d
Victor Manuel Polanco <vmp2408@gmail.com>
2017-10-13 18:07:40 -0400

    Changing default environment behavior

--- config/__init__.py
+++ config/__init__.py
@@ -12,13 +12,17 @@ BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def bootstrap_configuration():
     # TODO: change prints with logging
-    print("Bootstraping the configuration with the {} environment".format(os.environ['ACTIVE_ENVIRONMENT']))
+    # print("Bootstraping the configuration with the {} environment".format(os.environ['ACTIVE_ENVIRONMENT']))
     # Selected environment
     selected_environment = os.environ['ACTIVE_ENVIRONMENT'] if 'ACTIVE_ENVIRONMENT' in os.environ else 'default'
 
     # Set the environment module depending on the selected environment type
     active_config = "{}Config".format(selected_environment.title())
     active_environment = 'config.{}'.format(selected_environment)
+
+    if selected_environment == 'default':
+        active_config = 'Config'
+
     print("Pulling up the {} class from {}".format(active_config, active_environment))
     # Import module and instantiate class
     _module = import_module(active_environment)

bdda77c772d221433b3661ea02b958dd1d5f0a83
Victor Manuel Polanco <vmp2408@gmail.com>
2017-10-13 21:50:01 -0400

    Checking out post decorator

--- app/users/models.py
+++ app/users/models.py
@@ -4,11 +4,14 @@ from utils import db.BaseModel
 
 
 class User(BaseModel):
-    username = db.Column(db.String)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     email = db.Column(db.Email)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
 
 
 class UserSchema(ma.ModelSchema):
     class Meta:
-		model = User
+	model = User
 
--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 #!env/bin/python
 from app import app
 
+
+class TestResource:
+
+    @post
+    def some_method(some_arg):
+        return "Helloooo"
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='luxorv.io')
+    app.run(debug=True, host='luxorv.local')
